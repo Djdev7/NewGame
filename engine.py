5224b1d6d20a118665d59d92c3b506f13072e23a
Danny Johnson <djdevv7@gmail.com>
2016-02-09 01:18:46 -0500

    Ex45
    skeleton code for engine

---
+++ engine.py
@@ -0,0 +1,33 @@
+#The character starts in a dark room.He notices that he is strapped to a table.
+#------------------------------------------------------------------------------#
+#This is where i was place what needs to happen at this point
+#   1. Opening scene
+#   2. How to play
+#   3. Look at surroundings
+#   4. Descision tree
+#   5. Move to next node
+#   6. Display available nodes
+#   7. Display all avaiable commands
+#   8. View contents of inventory
+#------------------------------------------------------------------------------#
+# here we are gonna make the englne where the game state is.
+class Engine(object):
+#this dict will contain all the room description text
+	nodesd = {key: Value}
+# This function will display the current nodes discription.
+    def node_descrip(self, node):
+    	print Engine.nodes[]
+#this function will call the help dialog
+    def Help():
+    	if Userinput == "help" or "Help":
+			print ""
+#this function will move the engine to the next node
+    def next_node(self):
+    	if node_complete == True:
+    		
+
+    
+
+
+
+
